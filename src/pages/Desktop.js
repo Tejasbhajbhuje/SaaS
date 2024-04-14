93816b16e59ea68ca72c895e81881e87d60f3428
Tejas Bhajbhuje <tejasbhajbhuje0205@gmail.com>
2024-04-14 08:20:07 +0530

    change

---
+++ src/pages/Desktop.js
@@ -0,0 +1,384 @@
+import "./Desktop.css";
+
+const Desktop = () => {
+  return (
+    <div className="desktop">
+      <main className="stack">
+        <div className="bar">
+          <div className="stack1">
+            <div className="this-page-is">
+              This page is included in a free SaaS Website Kit.
+            </div>
+            <div className="version-is-here">
+              <div className="view-the-complete">View the complete Kit</div>
+              <img className="icons" alt="" src="/icons.svg" />
+            </div>
+          </div>
+        </div>
+        <header className="nav-bar-desktop">
+          <div className="logo">
+            <div className="logodefault">
+              <div className="logo-instance" />
+              <img
+                className="logosaas-icon"
+                loading="lazy"
+                alt=""
+                src="/logosaas@2x.png"
+              />
+            </div>
+            <div className="made-by">made by</div>
+            <img
+              className="black-horizontal-1-icon"
+              loading="lazy"
+              alt=""
+              src="/blackhorizontal-1.svg"
+            />
+          </div>
+          <div className="stack2">
+            <div className="about">About</div>
+            <div className="features">Features</div>
+            <div className="customers">Customers</div>
+            <div className="updates">Updates</div>
+            <div className="help">Help</div>
+            <div className="buttons">
+              <div className="get-for-free">Get for free</div>
+            </div>
+          </div>
+        </header>
+        <section className="pulse-shape">
+          <div className="hero">
+            <div className="hero-child" />
+            <div className="f-a-qs-form" />
+            <div className="hero-content">
+              <div className="tag">
+                <div className="version-20-is-here-parent">
+                  <input
+                    className="version-20-is"
+                    placeholder="Version 2.0 is here"
+                    type="text"
+                  />
+                  <div className="read-more-parent">
+                    <div className="read-more">Read more</div>
+                    <img className="icons1" alt="" src="/icons-1.svg" />
+                  </div>
+                </div>
+              </div>
+              <h1 className="h1">
+                <p className="one-task">One Task</p>
+                <p className="at-a-time">at a Time</p>
+              </h1>
+              <h3 className="body">
+                Celebrate the joy of accomplishment with an app designed to
+                track your progress, motivate your efforts, and celebrate your
+                successes.
+              </h3>
+              <button className="buttons1">
+                <div className="get-for-free1">Get for free</div>
+              </button>
+              <div className="your-company-inc">
+                <div className="wrapper-cursor-1">
+                  <img
+                    className="cursor-1-icon"
+                    loading="lazy"
+                    alt=""
+                    src="/cursor-1@2x.png"
+                  />
+                </div>
+                <img className="cursor-2-icon" alt="" src="/cursor-1@2x.png" />
+              </div>
+              <div className="wrapper-message-2-parent">
+                <div className="wrapper-message-2">
+                  <img
+                    className="message-2-icon"
+                    alt=""
+                    src="/message-2@2x.png"
+                  />
+                </div>
+                <img
+                  className="message-1-icon"
+                  loading="lazy"
+                  alt=""
+                  src="/message-2@2x.png"
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="stack-inner">
+          <div className="form-instance-parent">
+            <div className="form-instance">
+              <div className="trusted-by-the">
+                Trusted by the world’s most innovative teams
+              </div>
+            </div>
+            <div className="get-free-button-instance">
+              <img
+                className="acme-1-icon"
+                loading="lazy"
+                alt=""
+                src="/acme-1@2x.png"
+              />
+              <img
+                className="quantum-1-icon"
+                loading="lazy"
+                alt=""
+                src="/quantum-1@2x.png"
+              />
+              <div className="echo-1-wrapper">
+                <img
+                  className="echo-1-icon"
+                  loading="lazy"
+                  alt=""
+                  src="/echo-1@2x.png"
+                />
+              </div>
+              <div className="celestia-1-wrapper">
+                <img
+                  className="celestia-1-icon"
+                  loading="lazy"
+                  alt=""
+                  src="/celestia-1@2x.png"
+                />
+              </div>
+              <div className="pulse-1-wrapper">
+                <img
+                  className="pulse-1-icon"
+                  loading="lazy"
+                  alt=""
+                  src="/pulse-1@2x.png"
+                />
+              </div>
+              <div className="divider-shape">
+                <img
+                  className="apex-1-icon"
+                  loading="lazy"
+                  alt=""
+                  src="/apex-1@2x.png"
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="stack-child">
+          <div className="frame-parent">
+            <div className="footer-instance-wrapper">
+              <div className="footer-instance">
+                <h1 className="h2">Everything you need</h1>
+                <h3 className="body1">
+                  Enjoy customizable lists, team work tools, and smart tracking
+                  all in one place. Set tasks, get reminders, and see your
+                  progress simply and quickly.
+                </h3>
+              </div>
+            </div>
+            <div className="hero-content1">
+              <div className="feature-cards">
+                <button className="icons-wrapper">
+                  <img className="icons2" alt="" src="/icons-2.svg" />
+                </button>
+                <div className="f-a-qs">
+                  <b className="integration-ecosystem">Integration ecosystem</b>
+                  <div className="body2">
+                    Enhance your productivity by connecting with your favorite
+                    tools, keeping all your essentials in one place.
+                  </div>
+                </div>
+              </div>
+              <div className="feature-cards1">
+                <button className="icons-container">
+                  <img className="icons3" alt="" src="/icons-3.svg" />
+                </button>
+                <div className="integration-ecosystem-parent">
+                  <b className="integration-ecosystem1">
+                    Goal setting and tracking
+                  </b>
+                  <div className="body3">
+                    Define and track your goals, breaking down objectives into
+                    achievable tasks to keep your targets in sight.
+                  </div>
+                </div>
+              </div>
+              <div className="feature-cards2">
+                <button className="icons-frame">
+                  <img className="icons4" alt="" src="/icons-4.svg" />
+                </button>
+                <div className="integration-ecosystem-group">
+                  <b className="integration-ecosystem2">
+                    Secure data encryption
+                  </b>
+                  <div className="body4">
+                    With end-to-end encryption, your data is securely stored and
+                    protected from unauthorized access.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="footer-social-links">
+          <div className="footer-social-links-child" />
+          <div className="frame-group">
+            <div className="frame-wrapper">
+              <div className="h2-parent">
+                <h1 className="h21">Intuitive interface</h1>
+                <h3 className="body5">
+                  Celebrate the joy of accomplishment with an app designed to
+                  track your progress, motivate your efforts, and celebrate your
+                  successes, one task at a time.
+                </h3>
+              </div>
+            </div>
+            <div className="wrapper-app-night">
+              <img
+                className="app-night-icon"
+                loading="lazy"
+                alt=""
+                src="/app-night@2x.png"
+              />
+            </div>
+          </div>
+          <div className="footer-social-links-inner">
+            <div className="frame-container">
+              <div className="h2-wrapper">
+                <h1 className="h22">Frequently asked questions</h1>
+              </div>
+              <div className="faqs">
+                <div className="formfaq">
+                  <b className="how-does-the">
+                    How does the pricing work for teams
+                  </b>
+                  <div className="frame">
+                    <img
+                      className="app-name-logo"
+                      loading="lazy"
+                      alt=""
+                      src="/vector.svg"
+                    />
+                  </div>
+                </div>
+                <div className="n-a">
+                  <div className="formfaq1">
+                    <b className="how-does-the1">
+                      How does the pricing work for teams
+                    </b>
+                    <div className="frame1">
+                      <img
+                        className="vector-icon"
+                        loading="lazy"
+                        alt=""
+                        src="/vector-1.svg"
+                      />
+                    </div>
+                  </div>
+                  <div className="formfaq2">
+                    <b className="how-does-the2">
+                      How does the pricing work for teams
+                    </b>
+                    <div className="frame2">
+                      <img
+                        className="vector-icon1"
+                        loading="lazy"
+                        alt=""
+                        src="/vector-2.svg"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="formfaq3">
+                  <b className="how-does-the3">
+                    How does the pricing work for teams
+                  </b>
+                  <div className="frame3">
+                    <img className="vector-icon2" alt="" src="/vector-3.svg" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="n-a-wrapper">
+          <div className="n-a1">
+            <div className="n-a2">
+              <div className="n-a3">
+                <h1 className="h23">Get instant access</h1>
+                <div className="body6">
+                  Celebrate the joy of accomplishment with an app designed to
+                  track your progress and motivate your efforts.
+                </div>
+              </div>
+              <div className="form">
+                <input
+                  className="form1"
+                  placeholder="name@email.com"
+                  type="text"
+                />
+                <button className="buttons2">
+                  <div className="get-for-free2">Get access</div>
+                </button>
+              </div>
+            </div>
+            <img
+              className="emojistar-1-icon"
+              loading="lazy"
+              alt=""
+              src="/emojistar-1@2x.png"
+            />
+            <img
+              className="helix2-1-icon"
+              loading="lazy"
+              alt=""
+              src="/helix2-1@2x.png"
+            />
+          </div>
+        </section>
+        <footer className="footer-s">
+          <div className="n-a4">
+            <div className="your-company-inc1">
+              @ 2024 Your Company, Inc. All rights reserved
+            </div>
+          </div>
+          <div className="n-a5">
+            <img
+              className="socials-icon"
+              loading="lazy"
+              alt=""
+              src="/socials.svg"
+            />
+            <img
+              className="socials-icon1"
+              loading="lazy"
+              alt=""
+              src="/socials-1.svg"
+            />
+            <img
+              className="socials-icon2"
+              loading="lazy"
+              alt=""
+              src="/socials-2.svg"
+            />
+            <img
+              className="socials-icon3"
+              loading="lazy"
+              alt=""
+              src="/socials-3.svg"
+            />
+            <img
+              className="socials-icon4"
+              loading="lazy"
+              alt=""
+              src="/socials-4.svg"
+            />
+            <img
+              className="socials-icon5"
+              loading="lazy"
+              alt=""
+              src="/socials-5.svg"
+            />
+          </div>
+        </footer>
+      </main>
+    </div>
+  );
+};
+
+export default Desktop;
